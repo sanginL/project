72a4a3bd9a190d5640894b67c3a7f11297db860e
sanginL <96563787+sanginL@users.noreply.github.com>
2022-03-30 18:07:41 +0900

    Simple change using javascript
    Allows you to change the menu and footer more simply through JavaScript

---
+++ 전자저울판매사이트/index_1.js
@@ -0,0 +1,21 @@
+let product_ = ['고속계량라벨부착기', 'WM-AISP + PS-AI Series', 'WM-AISP Series', 'UNI-5L Series', 'UNI-CAP Series'];
+let img_ = ["고속계량라벨부착기.jpg", "WM-AISP+PS-AI_Series.jpg", "WM-AISP_Series.jpg",  "UNI-5L_Series.jpg", "UNI-CAP_Series-4.jpg"];
+let product_con = ['빠른 속도와 최첨단 기술을 사용한 라벨부착기','제조 라인형 자동Wrap포장기기', '계량, Wrap포장, 라벨부착. 3가지 작업을 한번에 해결','보급형 라벨프린팅 저울', '고급형 라벨프린팅저울.'];
+$(document).ready(function() {
+  for (let i = 0; i < 2; i++) {
+    let html_temp = `
+    <div class="product-list">
+<div class="product-box${i+1}" style="width: 220px; height: 400px; margin: 16.5px; border: 1px solid rgba(0, 0, 0, 0.2);" ><a href="product_${i+1}.html">
+  <div class="img-box">
+    <img src=${img_[i]} alt="">
+  </div>
+  <div class="text-box">
+    <div class="product_name" style="text-align : center; margin-bottom: 5px;">${product_[i]}</div>
+    <div class= "product_con" style="font-size : 12px; text-align : center;">${product_con[i]}</div>
+  </div></a>
+</div>
+</div>
+      `
+    $("#name-list").append(html_temp)
+  }
+})
--- 전자저울판매사이트/menu_bar.js
+++ 전자저울판매사이트/menu_bar.js
@@ -1,16 +1,40 @@
-
-// 상품 이미지 마우스오버시 이미지 변경. id를 이용하여 제한함.
-
-window.onload = function() {
-  var mainimg = document.getElementById('product_img');
-  var subimg = document.getElementById('subproduct_img');
-
-  var mainimg_ = mainimg.getElementsByTagName('img')[0];
-  var subimg_ = subimg.getElementsByTagName('img');
-
-  for(var i=0; i < subimg_.length; i++){
-    subimg_[i].onmouseover = function(){
-      mainimg_.src = this.src;
-    }
-  }
-}
+$(document).ready(function() {
+  let html_temp = `
+  <!-- <ul class="bagic_menu"> -->
+  <div class="dropdown">
+    <div class="dropbtn"><a href="index_1.html">전자저울</a></div>
+    <!-- <div class="dropdown-content">
+      <a href="product_1.html">1</a>
+      <a href="product_2.html">2</a>
+      <a href="#">3</a>
+    </div> -->
+  </div>
+  <div class="dropdown">
+    <div class="dropbtn"><a href="index_2.html">포장기계</a></div>
+    <!-- <div class="dropdown-content">
+      <a href="product_1.html">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+    </div> -->
+  </div>
+  <div class="dropdown">
+    <div class="dropbtn"><a href="index_3.html">라벨/기타</a></div>
+    <!-- <div class="dropdown-content">
+      <a href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+    </div> -->
+  </div>
+  <div class="dropdown">
+    <div class="dropbtn"><a href="index_4.html">중고</a></div>
+    <!-- <div class="dropdown-content">
+      <a href="#">1</a>
+      <a href="#">2</a>
+      <a href="#">3</a>
+    </div> -->
+  </div>
+  <div><a href="#">naver_cafe</a></div>
+<!-- </ul> -->
+    `
+  $("#menu_").append(html_temp)
+})
